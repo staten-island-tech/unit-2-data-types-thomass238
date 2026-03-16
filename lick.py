6695750eb45ea34be2a075356a74b4a3ff69c695
thomass238 <thomass238@nycstudents.net>
2026-03-16 10:17:11 -0400

    Create lick.py

---
+++ lick.py
@@ -0,0 +1,14 @@
+# x = 3
+# y = float(3)
+# print(x,y)
+
+# values = [1,2.23,5,7,2,30,15]
+# print(values[7])
+# for i in values:
+#     print(i)
+
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
